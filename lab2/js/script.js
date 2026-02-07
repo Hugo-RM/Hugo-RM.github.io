5f549eacba783bc0e0bf54352edb21b10cea72af
Hugo-RM <hugo.ruizmireles@gmail.com>
2026-02-06 19:55:32 -0800

    Updating to use on PC

--- lab2/js/script.js
+++ lab2/js/script.js
@@ -1,9 +1,45 @@
-const randomNum = 88;
+let ansText = document.querySelector("#ansTextBox");
+
+ansText.textContent = "Previous Guesses: ";
+
+const randomNum = (Math.floor(Math.random() * 99) % 100) + 1;
+let numGuesses = 0;
+let won = false;
 
 function guess() {
-    let guess = document.querySelector("#userGuess").value;
-    let ansText = document.querySelector("#ansTextBox");
-    ansText.textContent = guess;
+    if (numGuesses > 6 || won) {
+        return;
+    }
+
+    let userGuess = document.querySelector("#userGuess").value;
+    
+    if (!userGuess) {
+        return;
+    }
+
+    ansText.textContent += `${userGuess} `;
+    numGuesses++;
+    
+    let response = document.querySelector("#message");
+    
+    if (userGuess > randomNum) {
+        response.textContent = "Your guess was too high!";
+        response.style.color = "orange";
+    } else if (userGuess < randomNum) {
+        response.textContent = "Your guess was too low!";
+        response.style.color = "orange";
+    } else {
+        response.textContent = "CORRECT!";
+        response.style.color = "green";
+        won = true;
+        return;
+    }
+
+    if (numGuesses == 7) {
+        let endMessage = document.querySelector("#endGame");
+        endMessage.textContent = `You Lost, Number was: ${randomNum}`;
+        endMessage.style.color = "red";
+    }
 }
 
 document.querySelector("#guessButton").addEventListener("click", guess)
