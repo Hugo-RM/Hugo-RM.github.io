51b07b87d4c65391ee1a67665cc6c66ded0c3105
Hugo M. Ruiz-Mireles <hugo.ruizmireles@gmail.com>
2026-02-22 23:36:51 -0800

    lab 3

--- lab3/js/index.js
+++ lab3/js/index.js
@@ -3,7 +3,7 @@ document.querySelector("button").addEventListener("click", gradeQuiz);
 shuffleQ1Choices();
 
 function shuffleQ1Choices() {
-    let q1Choices = ["Monkey D. Luffy", "Roronoa Zoro", "Gold Roger", "Kaido"];
+    let q1Choices = ["Bogo Sort", "Bubble Sort", "Merge Sort", "BogoBogo Sort"];
     q1Choices = _.shuffle(q1Choices);
 
 
@@ -24,12 +24,71 @@ function shuffleQ1Choices() {
 }
 
 function gradeQuiz() {
-    let userAnswer1 = document.querySelector("input[name=q1]:checked").value;
-
-    if (userAnswer1 == "color") {
-        alert("CORRECT");
-    } else {
-        alert("INCORRECT");
+    if (!isFormValid()) {
+        return;
     }
+    
+    let score = 75;
+    
+    let userAnswer1 = document.querySelector("input[name=q1]:checked").value;
+    let userAnswer2 = document.querySelector("input[name=q2]").value.trim();
+    let userAnswer3 = document.querySelector("select[name=q3]").value;
+    let userAnswer4 = document.querySelector("input[name=q4]").value;
+    let userAnswer5 = document.querySelector("select[name=q5]").value;
+
+    if (userAnswer1 != "BogoBogo Sort") {
+        score -= 15;
+    }
+    if (userAnswer2.toLowerCase() != "binary search") {
+        score -= 15;
+    }
+    if (userAnswer3 != "Merge Sort") {
+        score -= 15;
+    }
+    if (userAnswer4 != "2") {
+        score -= 15;
+    }
+    if (userAnswer5 != "Stack") {
+        score -= 15;
+    }
+
+    document.querySelector("#totalScore").innerHTML = `SCORE: ${score} / 80`;
 }
 
+function isFormValid() {
+    let isValid = true;
+    let errorStr = "";
+
+    if (!document.querySelector("input[name=q1]:checked")) {
+        isValid = false;
+        errorStr += "Question 1 was not answered<br>";
+    }
+
+    if (document.querySelector("input[name=q2]").value.trim() == "") {
+        isValid = false;
+        errorStr += "Question 2 was not answered<br>";
+    }
+
+    if (document.querySelector("select[name=q3]").value == "") {
+        isValid = false;
+        errorStr += "Question 3 was not answered<br>";
+    }
+
+    if (document.querySelector("input[name=q4]").value == "") {
+        isValid = false;
+        errorStr += "Question 4 was not answered<br>";
+    }
+
+    if (document.querySelector("select[name=q5]").value == "") {
+        isValid = false;
+        errorStr += "Question 5 was not answered<br>";
+    }
+
+    if (!isValid) {
+        document.querySelector("#validationFdbk").innerHTML = errorStr;
+    } else {
+        document.querySelector("#validationFdbk").innerHTML = "";
+    }
+
+    return isValid;
+}
