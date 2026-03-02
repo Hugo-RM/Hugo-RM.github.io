90c553d8b4832fc98ecf39dc724c3e833175f249
Hugo M. Ruiz-Mireles <hugo.ruizmireles@gmail.com>
2026-03-01 21:56:47 -0800

    rebase

--- lab4/js/script.js
+++ lab4/js/script.js
@@ -2,8 +2,12 @@
 document.querySelector("#zip").addEventListener("change", displayCityInfo);
 document.querySelector("#state").addEventListener("click", displayStates);
 document.querySelector("#username").addEventListener("change", userNameCheck);
-document.querySelector(".password").addEventListener("click", suggestPassword);
-document.querySelector(".password").addEventListener("change", checkPasswordLength);
+document.querySelector("#password1").addEventListener("click", suggestPassword);
+document.querySelector("#password2").addEventListener("click", suggestPassword);
+document.querySelector("#password1").addEventListener("change", checkPassword1Validity);
+document.querySelector("#password2").addEventListener("change", checkPassword2Validity);
+document.querySelector("#password1").addEventListener("change", checkPasswordMatch);
+document.querySelector("#password2").addEventListener("change", checkPasswordMatch);
 
 // https://csumb.space/api/cityInfoAPI.php?zip=
 // https://csumb.space/api/allStatesAPI.php
@@ -11,6 +15,11 @@ document.querySelector(".password").addEventListener("change", checkPasswordLeng
 // https://csumb.space/api/usernamesAPI.php?username=
 // https://csumb.space/api/suggestedPassword.php?length=
 
+// global variables
+
+let statesDisplayed = false;
+let countiesDisplayed = false;
+let passwordSuggested = false;
 
 async function displayCityInfo() {
     try {
@@ -26,10 +35,16 @@ async function displayCityInfo() {
 
         let data = await response.json();
 
-        document.querySelector("#cityName").textContent = data.city;
-        document.querySelector("#cityLatitude").textContent = data.latitude;
-        document.querySelector("#cityLongitude").textContent = data.longitude;
+        console.log(data);
 
+        if (!data) {
+            document.querySelector("#invalidZip").textContent = "Invalid Input";
+            document.querySelector("#invalidZip").style.color = "red";;
+        } else {
+            document.querySelector("#cityName").textContent = data.city;
+            document.querySelector("#cityLatitude").textContent = data.latitude;
+            document.querySelector("#cityLongitude").textContent = data.longitude;
+        }
 
     } catch (err) {
         if (err instanceof TypeError) {
@@ -39,9 +54,6 @@ async function displayCityInfo() {
         }
     }
 }
-
-let statesDisplayed = false;
-let countiesDisplayed = false;
 
 async function displayStates() {
     try {
@@ -73,62 +85,7 @@ async function displayStates() {
             alert(err.message);
         }
     }
-    
-}
 
-async function userNameCheck() {
-    let username = document.querySelector("#username").value;
-    try {
-        let nameUrl = "https://csumb.space/api/usernamesAPI.php?username=" + username;
-        let response = await fetch(nameUrl);
-        if(!response.ok) {
-            alert("Error accessing API endpoint (username already exists)");
-        }
-        let data = await response.json();
-        if (!data.available) {
-            document.querySelector("#usernameMessage").textContent = "Username is not available";
-            document.querySelector("#usernameMessage").style.color = "red";
-        }
-        else {
-            document.querySelector("#usernameMessage").textContent = "";
-        }
-    }
-    catch (err) {
-        if (err instanceof TypeError) {
-            alert("Error accessing API endpoint (network failure)");
-        } else {
-            alert(err.message);
-        }
-    }
-}
-
-let passwordSuggested = false;
-
-async function suggestPassword() {
-    try {
-        if (passwordSuggested) return;
-
-        let url = "https://csumb.space/api/suggestedPassword.php?length=8";
-        let response = await fetch(url);
-
-        if (!response.ok) {
-            throw new Error("Error accessing API endpoint");
-        }
-
-        let data = await response.json();
-
-        let pElement = document.createElement("p");
-        pElement.textContent = data.password;
-        document.querySelector("#suggestedPassword").append(pElement);
-        
-        passwordSuggested = true;
-    } catch (err) {
-        if (err instanceof TypeError) {
-            alert("Error accessing API endpoint (network failure)");
-        } else {
-            alert(err.message);
-        }
-    }
 }
 
 async function displayCounties() {
@@ -141,7 +98,24 @@ async function displayCounties() {
         }
         let data = await response.json();
 
-        for (let countie of data){
+        if (Object.keys(data).length === 0) {
+            document.querySelector("#county").textContent = "";
+
+            let optionElement = document.createElement("option");
+            optionElement.textContent = "Select County";
+            document.querySelector("#county").append(optionElement);
+
+            document.querySelector("#noStateSelected").textContent = "Select a State";
+            document.querySelector("#noStateSelected").style.color = "red";
+
+            return;
+        }
+
+        document.querySelector("#county").textContent = "";
+        document.querySelector("#noStateSelected").textContent = "";
+
+
+        for (let countie of data) {
             let optionElement = document.createElement("option");
             optionElement.textContent = countie.county;
             optionElement.value = countie.county;
@@ -159,15 +133,100 @@ async function displayCounties() {
     console.log(state);
 }
 
-function checkPasswordLength() {
-    let currentPassword = document.querySelector(".password").value;
+async function userNameCheck() {
+    let username = document.querySelector("#username").value;
+    try {
+        let nameUrl = "https://csumb.space/api/usernamesAPI.php?username=" + username;
 
-    if (currentPassword.length < 6) {
-        let pElement = document.createElement("p");
-        pElement.textContent = "Password too short (Must be at least 6 characters long)!";
-        pElement.style.color = "red";
-        document.querySelector("#suggestedPassword").append(pElement);
-    } else {
-        document.querySelector("#suggestedPassword").textContent = "";
+        console.log(username);
+        console.log(nameUrl);
+
+        let response = await fetch(nameUrl);
+
+        if (!response.ok) {
+            throw new Error("Error accessing API endpoint");
+        }
+
+        let data = await response.json();
+
+        if (!data.available) {
+            document.querySelector("#usernameMessage").textContent = "Username is not available";
+            document.querySelector("#usernameMessage").style.color = "red";
+        } else if (username.length < 4) {
+            document.querySelector("#usernameMessage").textContent = "Must be at least 3 characters long";
+            document.querySelector("#usernameMessage").style.color = "red";
+        } else {
+            document.querySelector("#usernameMessage").textContent = "Username is available";
+            document.querySelector("#usernameMessage").style.color = "green";
+        }
+    }
+    catch (err) {
+        alert(err.message);
+    }
+}
+
+async function suggestPassword() {
+    try {
+        if (passwordSuggested) return;
+
+        let url = "https://csumb.space/api/suggestedPassword.php?length=8";
+        let response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error("Error accessing API endpoint");
+        }
+
+        let data = await response.json();
+
+        document.querySelector("#suggestedPassword").textContent = `Suggested Password: ${data.password}`;
+
+        passwordSuggested = true;
+    } catch (err) {
+        if (err instanceof TypeError) {
+            alert("Error accessing API endpoint (network failure)");
+        } else {
+            alert(err.message);
+        }
+    }
+}
+
+function checkPassword1Validity() {
+    return checkPasswordValidity("#password1", "#invalidPassword1");
+}
+
+function checkPassword2Validity() {
+    return checkPasswordValidity("#password2", "#invalidPassword2");
+}
+
+function checkPasswordValidity(passwordId, invalidPasswordId) {
+    let currentPassword = document.querySelector(passwordId).value;
+    let pElement = document.querySelector(invalidPasswordId);
+
+    return checkPasswordLength(currentPassword, pElement);
+}
+
+function checkPasswordLength(password, pElement) {
+    if (password.length >= 6) {
+        pElement.textContent = "";
+        return true;
+    }
+
+    pElement.textContent = "Password too short (Must be at least 6 characters long)!";
+    pElement.style.color = "red";
+
+    return false;
+}
+
+function checkPasswordMatch() {
+    let password1 = document.querySelector("#password1").value;
+    let password2 = document.querySelector("#password2").value;
+    let validLen = checkPassword1Validity() && checkPassword2Validity();
+
+    if (validLen && password1 === password2) {
+        document.querySelector("#passwordMatch").textContent = "valid password";
+        document.querySelector("#passwordMatch").style.color = "green";
+    } else if (validLen) {
+        document.querySelector("#passwordMatch").textContent = "Password must match";
+        document.querySelector("#passwordMatch").style.color = "red";
     }
 }
